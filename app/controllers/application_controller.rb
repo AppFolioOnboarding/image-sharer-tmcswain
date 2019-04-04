6075e96c0c360874994382637c3f2c947148eccd
Taylor McSwain <taylor@mcswain.cc>
2019-04-04 15:27:25 -0700

    tm - display all images on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def home; end
+  def home
+    @urls = Image.all.order(created_at: :desc).map(&:url)
+  end
 end
