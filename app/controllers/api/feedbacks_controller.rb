ca4c38d0f1afbd0e6f51320a4fbd33fc17ed8213
Taylor McSwain <taylor@mcswain.cc>
2019-04-15 14:31:00 -0700

    tm - add Create method for Feedback

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,8 @@
 module Api
   class FeedbacksController < ApplicationController
     def create
-      # Implement your create action
+      Feedback.create!(params.require(:feedback).permit(:username, :comments))
+      redirect_to :new_feedback
     end
   end
 end
